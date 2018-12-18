397b3ba6bfa602ee08299e13525d7caad1c7dc28
BaharehNiknia <Bahareh.Niknia@gmail.com>
2018-12-18 05:19:37 -0800

    require express

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var app = express();
+//var things=require("./things.js");
+//app.use('/things',things);
 
-app.get('/server', function(req, res){
-   res.send("Hello World!");
+
+app.get('/things/:id', function(req, res){
+   res.send('The id you specified is ' + req.params.id);
 });
+//Other routes here
+app.get('*', function(req, res){
+    res.send('Sorry, this is an invalid URL.');
+ });
 
-app.listen(3004);
+app.listen(3000);
+
+
+
+//app.get('/server', function(req, res){
+//   res.send("Hello World 11!");
+//});
+//app.post('/server', function(req, res){
+ //  res.send("Post called!");
+//});
+//app.all('/server', function(req, res){
+ //  res.send("HTTP method doesn't have any effect on this route!");
+//});
+
+
+//app.listen(3004);
