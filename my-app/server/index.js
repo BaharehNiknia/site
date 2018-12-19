cc19822306d30fbad10caf17f09cd5498ce2e3f1
BaharehNiknia <Bahareh.Niknia@gmail.com>
2018-12-19 00:14:06 -0800

    pug

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -3,14 +3,38 @@ var app = express();
 //var things=require("./things.js");
 //app.use('/things',things);
 
+app.set('view engine', 'pug');
+app.set('views','./views');
 
-app.get('/things/:id', function(req, res){
-   res.send('The id you specified is ' + req.params.id);
+app.get('/components', function(req, res){
+   res.render('content');
 });
+
+//app.get('/dynamic', function(req, res){
+//   res.render('dynamic',{
+//      user: {name: "Ayush", age: "20"}
+ //  });
+//});
+
+//app.get('/first_template', function(req, res){
+//   res.render('first_view');
+//});
+
+//app.get('/dynamic_view', function(req, res){
+//   res.render('dynamic', {
+ //     name: "TutorialsPoint", 
+ //     url:"http://www.tutorialspoint.com"
+ //  });
+//});
+
+
+//app.get('/things/:id', function(req, res){
+//   res.send('The id you specified is ' + req.params.id);
+//});
 //Other routes here
-app.get('*', function(req, res){
-    res.send('Sorry, this is an invalid URL.');
- });
+//app.get('*', function(req, res){
+ //   res.send('Sorry, this is an invalid URL.');
+ //});
 
 app.listen(3000);
 
