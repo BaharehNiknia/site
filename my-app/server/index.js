b56d991c1fc1186ee8da91db4fbbe3867ed01b54
BaharehNiknia <Bahareh.Niknia@gmail.com>
2018-12-20 04:22:13 -0800

    in way of mongodb

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,32 +1,46 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
-var app = express();
-
-app.get('/', function(req, res){
-   res.render('form');
-});
-
+var express=require('express');
+var app=express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_db');
 app.set('view engine', 'pug');
-app.set('views', './views');
-
-// for parsing application/json
-app.use(bodyParser.json()); 
-
-// for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true })); 
-//form-urlencoded
-
-// for parsing multipart/form-data
-app.use(upload.array()); 
-app.use(express.static('public'));
-
-app.post('/', function(req, res){
-   console.log(req.body);
-   res.send("recieved your request!");
+app.set('views','./views');
+var personSchema = mongoose.Schema({
+   name: String,
+   age: Number,
+   nationality: String
 });
-app.listen(3000);
+var Person = mongoose.model("Person", personSchema);
+app.get('/person', function(req, res){
+   res.render('person');
+});
+app.post('/person', function(req, res){
+   var personInfo = req.body; //Get the parsed information
+   console.log("hi");
+   console.log(personInfo);
+   if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+      res.render('show_message', {
+         message: "Sorry, you provided worng info", type: "error"});
+   } else {
+      var newPerson = new Person({
+         name: personInfo.name,
+         age: personInfo.age,
+         nationality: personInfo.nationality
+      });
+		
+      newPerson.save(function(err, Person){
+         if(err)
+            res.render('show_message', {message: "Database error", type: "error"});
+         else
+            res.render('show_message', {
+               message: "New person added", type: "success", person: personInfo});
+      });
+   }
+});
+
+Person.find(function(err, response){
+   console.log(response);
+});
+
 
 
 
@@ -75,7 +89,7 @@ app.listen(3000);
  //   res.send('Sorry, this is an invalid URL.');
  //});
 
-//app.listen(3000);
+app.listen(3000);
 
 
 
