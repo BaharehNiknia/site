3e5846613e3ef37b11faf5d488b93aa72a9abf3d
BaharehNiknia <Bahareh.Niknia@gmail.com>
2019-01-03 05:25:15 -0800

    cmt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import Car from './components/common/Carousel.js'
 import Btn from './components/common/Button.js'
 import Grd from "./components/common/Grd.js"
 import BrdC from "./components/common/BrdC.js"
+
 import './App.css';
 
 class Form extends React.Component{
   render(){
     return (
  <fragment>
+  
   <BrdC></BrdC>
   <Car ></Car> 
    <Btn></Btn>
